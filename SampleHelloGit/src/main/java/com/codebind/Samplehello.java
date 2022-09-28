4520ccd87d8e615604fb5c54ba903248baf5fda0
AnanyaMisra07 <ananyamisra97021@gmail.com>
2022-09-29 00:06:55 +0530

    changed Samplehello.java file

--- SampleHelloGit/src/main/java/com/codebind/Samplehello.java
+++ SampleHelloGit/src/main/java/com/codebind/Samplehello.java
@@ -3,6 +3,7 @@ package com.codebind;
 public class Samplehello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git");
+                                   System.out.println("Hello,Everyone");
 	}
 
 }
